48d7007bd742493fae62a98d7c2dea5d80fe623e
Redox <mail@redox.to>
2022-12-04 20:19:01 +0100

    Fix includes

--- lua/grutto/client/cl_init.lua
+++ lua/grutto/client/cl_init.lua
@@ -1,12 +1,12 @@
 GRUTTO = {}
 
-include( "panels/grutto_editor.lua" )
-include( "panels/grutto_editor_tabs.lua" )
-include( "panels/grutto_console.lua" )
-include( "panels/grutto_sidebar.lua" )
-include( "panels/grutto_topbar.lua" )
-include( "cl_runcode.lua" )
-include( "cl_autocomplete.lua" )
-include( "cl_editor.lua" )
+include( "grutto/client/panels/grutto_editor.lua" )
+include( "grutto/client/panels/grutto_editor_tabs.lua" )
+include( "grutto/client/panels/grutto_console.lua" )
+include( "grutto/client/panels/grutto_sidebar.lua" )
+include( "grutto/client/panels/grutto_topbar.lua" )
+include( "grutto/client/cl_runcode.lua" )
+include( "grutto/client/cl_autocomplete.lua" )
+include( "grutto/client/cl_editor.lua" )
 
 print( "CL LuaEditor loaded." )
